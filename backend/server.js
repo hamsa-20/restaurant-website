f08bfa8c557ee824daf71a07dd08785936dc94b2
hamsa <hamsadixit470@gmail.com>
2025-02-23 23:27:09 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
+// import app from "./app.js";
+
+// app.listen(process.env.PORT,()=>{
+//     console.log(`server running on Port ${process.env.PORT}`);
+// }
+
+// );
+import dotenv from "dotenv";
 import app from "./app.js";
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server running on Port ${process.env.PORT}`);
-}
+// Load environment variables from .env file
+dotenv.config();
 
-);
+const PORT = process.env.PORT || 4000; // Default to 4000 if not set
+
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}`);
+});
